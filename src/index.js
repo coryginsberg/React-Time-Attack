cbec4314941f2e3258d7dcb327201c6d662d7555
Cory Ginsberg <cory.ginsberg@perficient.com>
2018-08-01 16:26:05 -0500

    Leaving this alone for now. Gonna follow a different tutorial and come back to it later

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ class LoginControl extends React.Component {
   constructor(props) {
     super(props);
     this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
     this.state = {isGameStart: false};
   }
 
@@ -17,6 +16,7 @@ class LoginControl extends React.Component {
   handleLogoutClick() {
     this.setState({isGameStart: false});
   }
+  
 
   render() {
     const isGameStart = this.state.isGameStart;
@@ -38,32 +38,30 @@ function WelcomeText(props) {
   return <h1>Welcome to Time Attack!</h1>;
 }
 
-// function GuestGreeting(props) {
-//   return <h1>Press START to play!</h1>;
-// }
+function RunGame(props) {
+  return (
+    <h1>Game starts in 3</h1>
+  );
+}
 
 function Greeting(props) {
   const isGameStart = props.isGameStart;
   if (!isGameStart) {
     return <WelcomeText />;
+  } else {
+    return <RunGame />;
   }
 }
 
 function StartGame(props) {
+  const style = this.state.isGameStart ? {display : 'none'} : {};
+
   return (
-    <button onClick={props.onClick}>
+    <button onClick={props.onClick} style={style}>
       START
     </button>
   );
 }
-
-// function LogoutButton(props) {
-//   return (
-//     <button onClick={props.onClick}>
-//       Logout
-//     </button>
-//   );
-// }
 
 ReactDOM.render(
   <LoginControl />,
