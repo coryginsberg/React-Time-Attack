59944147bd3bc70d626e1dc87e33583748a1ef3a
Cory Ginsberg <cory.ginsberg@perficient.com>
2018-08-01 15:34:49 -0500

    Changed base from default 'create-react-app' to the tutorial file found on the ReactJS website.

--- src/index.js
+++ src/index.js
@@ -2,120 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isGameStart: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isGameStart: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isGameStart: false});
+  }
+
+  render() {
+    const isGameStart = this.state.isGameStart;
+    let button;
+
+    button = <StartGame onClick={this.handleLoginClick} />;
+    
+
+    return (
+      <div>
+        <Greeting isGameStart={isGameStart} />
+        {button}
+      </div>
+    );
+  }
+}
+
+function WelcomeText(props) {
+  return <h1>Welcome to Time Attack!</h1>;
+}
+
+// function GuestGreeting(props) {
+//   return <h1>Press START to play!</h1>;
+// }
+
+function Greeting(props) {
+  const isGameStart = props.isGameStart;
+  if (!isGameStart) {
+    return <WelcomeText />;
+  }
+}
+
+function StartGame(props) {
   return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
+    <button onClick={props.onClick}>
+      START
     </button>
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square 
-        value={this.state.squares[i]}
-        onClick={()=> this.handleClick(i)}
-      />
-      );
-  }
-
-  render() {
-
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
+// function LogoutButton(props) {
+//   return (
+//     <button onClick={props.onClick}>
+//       Logout
+//     </button>
+//   );
+// }
 
 ReactDOM.render(
-  <Game />,
+  <LoginControl />,
   document.getElementById('root')
 );
+
